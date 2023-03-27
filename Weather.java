ca717bc2b6de598f3a0359bb437428ffdb1f89e7
Vivian Jiaying Ren <vren2@uwo.ca>
2023-03-27 20:41:21 +0100

    yes

---
+++ Weather.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cs2212;
+
+/**
+ *
+ * @author rrenv
+ */
+public class Weather {
+    
+    private String city;
+    private int currWeather;
+    private String currCondition;
+    
+    public Weather (String city) {
+        this.city = city;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public int getCurrWeather() {
+        return currWeather;
+    }
+    
+    public String getCurrCondition() {
+        return currCondition;
+    }
+    
+    public void updateCurrWeather() {
+        
+    }
+    
+    public void updateCurrCondition() {
+        
+    }
+    
+    
+    
+    
+}
