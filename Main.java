c119f47b17a447a1c28d39aa33a7f89577e5300b
Vivian Jiaying Ren <vren2@uwo.ca>
2023-03-29 04:48:35 +0100

    yes

--- Main.java
+++ Main.java
@@ -5,45 +5,93 @@
 package com.cs2212;
 import java.io.*;
 import java.util.*;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+//import org.json.simple.*;
+//import org.json.simple.parser.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 /**
  *
  * @author rrenv
  */
-public class Main {
+public class Main extends JFrame {
     
-    public static void main(String[] args) {
+    private JFrame mainFrame;
+    
+    public Main() {
         
-        // Load log in screen and check user credentials
-        
-        Campus campus = new Campus("Western University", "1151 Richmond Street, London");
-        
-        // Load built in POIs from JSON
-        JSONParser parser = new JSONParser();
-                        
-        try {
-           Object obj = parser.parse(new FileReader("C:/Users/JOYZH/OneDrive/Documents/NetBeansProjects/WesternMap/src/main/java/com/cs2212/poi.json"));
-           JSONObject jsonObject = (JSONObject)obj;
-           JSONArray pois = (JSONArray) jsonObject.get("pois");
+        mainFrame = new JFrame();      
+      
+        Border blackline = BorderFactory.createLineBorder(Color.black);
 
-           for(Object o : pois) {
-                JSONObject poi = (JSONObject) o;
-                long layerId = (long)poi.get("layerid");
-                long xCoord = (long)poi.get("xcoord");
-                long yCoord = (long)poi.get("ycoord");
-                String roomNum = (String)poi.get("roomnum");
-                String name = (String)poi.get("name");
-                String description = (String)poi.get("description");
-                boolean builtIn = (boolean)poi.get("builtin");
-  
-                POI newPoi = new POI(0, layerId, xCoord, yCoord, roomNum, name, description, builtIn);
-           }
-        } catch(Exception e) {
-            System.out.println("error");
-           e.printStackTrace();
-        }
+        JPanel favourites = new JPanel();
+        favourites.setBounds(1000,0,200,80);
+        favourites.setBorder(blackline);
+        JLabel favouritesLabel = new JLabel("Favourites");
+        favouritesLabel.setBounds(1000, 0, 20, 20);
+        favourites.add(favouritesLabel);
+        mainFrame.add(favourites);
+             
+        JPanel created = new JPanel();
+        created.setBounds(1000,80,200,80);
+        JLabel createdLabel = new JLabel("Created POIs");
+        createdLabel.setBounds(1000, 80, 20, 20);
+        mainFrame.add(created);
+        mainFrame.add(createdLabel);
+        
+        JPanel classrooms = new JPanel();
+        classrooms.setBounds(1000,160,200,80);
+        JLabel classroomLabel = new JLabel("Created POIs");
+        classroomLabel.setBounds(1000, 160, 20, 20);
+        mainFrame.add(classrooms);
+        mainFrame.add(classroomLabel);
+        
+        JPanel navigation = new JPanel();
+        JPanel washrooms = new JPanel();
+        JPanel entryExit = new JPanel();
+        JPanel genLabs = new JPanel();
+        JPanel resturaunts = new JPanel();
+        JPanel csSpecfic = new JPanel();
+        
+        mainFrame.setSize(1200,800);//400 width and 500 height  
+        mainFrame.setLayout(null);//using no layout managers  
+        mainFrame.setVisible(true);//making the frame visible   
+
+        
+//        Campus campus = new Campus("Western University", "1151 Richmond Street, London");
+        
+//        // Load built in POIs from JSON
+//        JSONParser parser = new JSONParser();
+//                        
+//        try {
+//           Object obj = parser.parse(new FileReader("poi.json"));
+//           JSONObject jsonObject = (JSONObject)obj;
+//           JSONArray pois = (JSONArray) jsonObject.get("pois");
+//
+//           for(Object o : pois) {
+//                JSONObject poi = (JSONObject) o;
+//                long layerId = (long)poi.get("layerid");
+//                long xCoord = (long)poi.get("xcoord");
+//                long yCoord = (long)poi.get("ycoord");
+//                String roomNum = (String)poi.get("roomnum");
+//                String name = (String)poi.get("name");
+//                String description = (String)poi.get("description");
+//                boolean builtIn = (boolean)poi.get("builtin");
+//  
+//                POI newPoi = new POI(0, layerId, xCoord, yCoord, roomNum, name, description, builtIn);
+//           }
+//        } catch(Exception e) {
+//            System.out.println("error");
+//           e.printStackTrace();
+//        }
         
         // Load user data from JSON
         
@@ -55,9 +103,9 @@ public class Main {
    
     }
     
-    public POI getPOI(int poiId) {
-        
-    }
+//    public POI getPOI(int poiId) {
+//        
+//    }
     
     public boolean deletePOI(int poiId) {
         return true;
