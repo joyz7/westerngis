bf40dadf51edbf3c1c42cbe076ede32be2760756
Vivian Jiaying Ren <vren2@uwo.ca>
2023-04-07 23:33:23 +0100

    my work

--- Building.java
+++ Building.java
@@ -7,9 +7,13 @@ package com.cs2212;
 import java.util.*;
 
 /**
+ * A Building represents a structure containing a number of Floors.
+ * Floors can be added to the building via the addFloor() method.
+ * The building maintains a List of Floors as well as a Map of Floors keyed by name.
  *
  * @author JOYZH
  */
+
 public class Building {
     private Map<String,List<Floor>> floors;
     private List<Floor> array;
@@ -17,6 +21,13 @@ public class Building {
     private String address;
     private Integer numFloors;
     
+     /**
+     * Constructs a new Building with the specified name, address, and number of floors.
+     * Initializes the floors map and array list.
+     * @param name the name of the building
+     * @param address the address of the building
+     * @param numFloors the number of floors in the building
+     */
     public Building(String name, String address, Integer numFloors) {
         floors = new HashMap<>();
         array = new ArrayList<>();
@@ -25,23 +36,44 @@ public class Building {
         this.numFloors = numFloors;
     }
     
+     /**
+     * Adds a Floor to the Building.
+     * The Floor is added to the List of Floors and the Map of Floors keyed by the Building's name.
+     * @param floor the Floor to add to the Building
+     */
     public void addFloor(Floor floor) {
         array.add(floor);
         floors.put(name,array);
     }
     
+    /**
+     * Returns the Map of Floors keyed by the Building's name.
+     * @return the Map of Floors keyed by the Building's name
+     */
     public Map getFloors() {
         return floors;
     }
     
+    /**
+     * Returns the List of Floors.
+     * @return the List of Floors
+     */
     public List<Floor> getArray() {
         return array;
     }
     
+    /**
+     * Returns the number of floors in the Building.
+     * @return the number of floors in the Building
+     */
     public Integer getNumFloors() {
         return this.numFloors;
     }
     
+     /**
+     * Returns an Object array containing the numbers of the Floors in the Building.
+     * @return an Object array containing the numbers of the Floors in the Building
+     */
     public Object[] getFloorsArray() {
         Object[] floorNum = new Object[array.size()];
         for (int i=0; i<array.size(); i++) {
@@ -49,11 +81,19 @@ public class Building {
         }
         return floorNum;
     }
-        
+       
+    /**
+     * Returns the name of the Building.
+     * @return the name of the Building
+     */
     public String getName() {
         return name;
     }
     
+     /**
+     * Returns the address of the Building.
+     * @return the address of the Building
+     */
     public String getAddress() {
         return address;
     }
